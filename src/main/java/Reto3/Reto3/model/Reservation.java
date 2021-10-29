aae45e48204d2de078b7a5e4a244d1e8b366d57f
lizethMarti <lizeth.martinez.mt@correo.usa.edu.co>
2021-10-28 20:49:46 -0500

    corrección cors

--- src/main/java/Reto3/Reto3/model/Reservation.java
+++ src/main/java/Reto3/Reto3/model/Reservation.java
@@ -29,17 +29,33 @@ public class Reservation implements Serializable {
      * Función para que el id sea generado automaticamente
      */
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /**
+     * Atributo id
+     */
     private Integer idReservation;
+    /**
+     * Atributo startDate
+     */
     private Date startDate;
+    /**
+     * Atributo devolutionDate
+     */
     private Date devolutionDate;
+    /**
+     * Atributo status
+     */
     private String status = "created";
     
     /**
      * Llave Foranea para la unión con a tabla room
      */
     @ManyToOne
+    //como se va a llamar la llave foranea
     @JoinColumn(name = "id")
     @JsonIgnoreProperties("reservations")
+    /**
+     * Estamos llamando al objeto Room (atributo)
+     */
     private Room room;
 
     /**
@@ -48,65 +64,124 @@ public class Reservation implements Serializable {
     @ManyToOne
     @JoinColumn(name = "idClient")
     @JsonIgnoreProperties({"reservations", "messages"})
+    /**
+     * Estamos llamando al objeto Client
+     */
     private Client client;
     
+    /**
+     * Atributo score
+     */
     private String score;
 
     /**
-     * Getter y Setter
+     * Permite acceder al valor del atributo 
+     * @return room
      */
     public Room getRoom() {
         return room;
     }
 
+    /**
+     *Permite asignar un valor al atributo
+     * @param room
+     */
     public void setRoom(Room room) {
         this.room = room;
     }
 
+    /**
+     * Permite acceder al valor del atributo 
+     * @return client
+     */
     public Client getClient() {
         return client;
     }
 
+    /**
+     *Permite asignar un valor al atributo
+     * @param client
+     */
     public void setClient(Client client) {
         this.client = client;
     }
 
+    /**
+     * Permite acceder al valor del atributo 
+     * @return idReservation
+     */
     public Integer getIdReservation() {
         return idReservation;
     }
 
+    /**
+     *Permite asignar un valor al atributo
+     * @param idReservation
+     */
     public void setIdReservation(Integer idReservation) {
         this.idReservation = idReservation;
     }
 
+    /**
+     * Permite acceder al valor del atributo 
+     * @return startDate
+     */
     public Date getStartDate() {
         return startDate;
     }
 
+    /**
+     *Permite asignar un valor al atributo
+     * @param startDate 
+     */
     public void setStartDate(Date startDate) {
         this.startDate = startDate;
     }
 
+    /**
+     * Permite acceder al valor del atributo 
+     * @return DevolutionDate
+     */
     public Date getDevolutionDate() {
         return devolutionDate;
     }
 
+    /**
+     *Permite asignar un valor al atributo
+     * @param devolutionDate
+     */
     public void setDevolutionDate(Date devolutionDate) {
         this.devolutionDate = devolutionDate;
     }
 
+    /**
+     * Permite acceder al valor del atributo 
+     * @return status
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     *Permite asignar un valor al atributo
+     * @param status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
     
+    /**
+     * Permite acceder al valor del atributo 
+     * @return score
+     */
     public String getScore() {
         return score;
     }
 
+    /**
+     *Permite asignar un valor al atributo
+     * @param score
+     */
     public void setScore(String score) {
         this.score = score;
     }
